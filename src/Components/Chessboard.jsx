8f2a1ad980b9c577664f69df8a676136ac629e3b
Sajed Azizi <sajedazizi@sajeds-air.lan>
2022-07-04 23:09:29 +0100

    rendered all pieces on board

--- src/Components/Chessboard.jsx
+++ src/Components/Chessboard.jsx
@@ -7,19 +7,52 @@ const Chessboard = () => {
     const xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     const yAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
+    const pieces = [];
+
+    for(let i = 0; i < 8; i++){
+        pieces.push({image:"/Assets/Pieces/pawn_b.png", x:i, y:6});
+    }
+    for(let i = 0; i < 8; i++){
+        if(i<=4){
+            pieces.push({image:`/Assets/Pieces/${i}.png`, x:i, y:7});
+        } else {
+            pieces.push({image:`/Assets/Pieces/${-i+7}.png`, x:i, y:7});
+        }
+    }
+
+    for(let i = 0; i < 8; i++){
+        pieces.push({image:"/Assets/Pieces/pawn_w.png", x:i, y:1});
+    }
+    for(let i = 0; i < 8; i++){
+        if(i<=4){
+            pieces.push({image:`/Assets/Pieces/${i}w.png`, x:i, y:0});
+        } else {
+            pieces.push({image:`/Assets/Pieces/${-i+7}w.png`, x:i, y:0});
+        }
+    }
+    
+
+
     let Board = [];
     // yAxis.map((y) => {xAxis.map((x)=> Board.push(<div className={styles.tile}>{x}{y} </div>))});
 
     for(let i = yAxis.length -1; i>=0; i--){
         for(let j= 0; j< xAxis.length; j++){
             const number = i + j;
+            let image = undefined;
 
-            Board.push(<Tile number={number}/>)
+            pieces.forEach((p)=> {
+                if(p.x === j && p.y === i){
+                    image = p.image
+                }
+            })
+
+            Board.push(<Tile key={`${j},${i}`} image={image} number={number}/>)
         }
     }
     
-    let letters = xAxis.map((x)=> <div>{x}</div>)
-    let numbers = yAxis.map((y)=> <div>{y}</div>)
+    let letters = xAxis.map((x,i)=> <div key={i}>{x}</div>)
+    let numbers = yAxis.map((y,j)=> <div key={j}>{y}</div>)
   return (
     <div className = {styles.fullboard}>
         <div className={styles.numbers}>
--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import { useState } from 'react';
 import styles from './Styles/Tile.module.scss';
 
 const Tile = (props) => {
-
-    //const [tile, setTile] = useState(<div>{props.number}</div>);
     
     if(props.number % 2 === 0){
-        return <div className={styles.blackTile}></div>;
+        return <div className={styles.blackTile}><img src={props.image}/></div>;
     } else {
-        return <div className={styles.whiteTile}></div>;
+        return <div className={styles.whiteTile}><img src={props.image}/></div>;
     }
 }
 
