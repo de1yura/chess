4ae7474bc5fad824e5e7b1b6dcf0784c508305a1
Sajed Azizi <sajedazizi@Sajeds-Air.net>
2022-07-10 01:46:00 +0100

    pieces move only in board and snap into tile

--- src/Components/Chessboard.jsx
+++ src/Components/Chessboard.jsx
@@ -1,41 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Styles/Chessboard.module.scss';
+import { useState } from 'react';
 import Tile from './Tile';
 
 const Chessboard = () => {
 
+    const boardRef = useRef(null);
     const xAxis = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
     const yAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
+    const starting = [];
+    const [gridX, setGridX] = useState(0);
+    const [gridY, setGridY] = useState(0);
 
-    const pieces = [];
 
     for(let i = 0; i < 8; i++){
-        pieces.push({image:"/Assets/Pieces/pawn_b.png", x:i, y:6});
+        starting.push({image:"/Assets/Pieces/pawn_b.png", x:i, y:6});
     }
     for(let i = 0; i < 8; i++){
         if(i<=4){
-            pieces.push({image:`/Assets/Pieces/${i}.png`, x:i, y:7});
+            starting.push({image:`/Assets/Pieces/${i}.png`, x:i, y:7});
         } else {
-            pieces.push({image:`/Assets/Pieces/${-i+7}.png`, x:i, y:7});
+            starting.push({image:`/Assets/Pieces/${-i+7}.png`, x:i, y:7});
         }
     }
 
     for(let i = 0; i < 8; i++){
-        pieces.push({image:"/Assets/Pieces/pawn_w.png", x:i, y:1});
+        starting.push({image:"/Assets/Pieces/pawn_w.png", x:i, y:1});
     }
     for(let i = 0; i < 8; i++){
         if(i<=4){
-            pieces.push({image:`/Assets/Pieces/${i}w.png`, x:i, y:0});
+            starting.push({image:`/Assets/Pieces/${i}w.png`, x:i, y:0});
         } else {
-            pieces.push({image:`/Assets/Pieces/${-i+7}w.png`, x:i, y:0});
+            starting.push({image:`/Assets/Pieces/${-i+7}w.png`, x:i, y:0});
         }
     }
+
+    const [pieces, setPieces] = useState(starting);
+    
     
 
-
+//Placing the Pieces on the board
     let Board = [];
-    // yAxis.map((y) => {xAxis.map((x)=> Board.push(<div className={styles.tile}>{x}{y} </div>))});
-
     for(let i = yAxis.length -1; i>=0; i--){
         for(let j= 0; j< xAxis.length; j++){
             const number = i + j;
@@ -52,14 +57,91 @@ const Chessboard = () => {
     }
     
     let letters = xAxis.map((x,i)=> <div key={i}>{x}</div>)
-    let numbers = yAxis.map((y,j)=> <div key={j}>{y}</div>)
+    let numbers = yAxis.map((y,j)=> <div key={j }>{y}</div>)
+
+    const [active, setActive] = useState(false);
+
+    const grabPiece = (e) => {
+        const element = e.target;
+        if(element.className.includes("piece")){
+            setGridX(Math.floor((e.clientX - boardRef.current.offsetLeft) / 100));
+            setGridY(Math.abs(Math.ceil((e.clientY - boardRef.current.offsetTop - 800) / 100)));
+
+            const x = e.clientX - 0.5*(element.clientHeight);
+            const y = e.clientY - 0.5*(element.clientHeight);
+
+            element.style.position='absolute';
+            element.style.left = `${x}px`;
+            element.style.top = `${y}px`;
+            setActive(true);
+        }
+    }
+
+    const movePiece = (e) => {
+        const element = e.target;
+        if(element.className.includes("piece") && active && boardRef.current){
+            const minX = boardRef.current.offsetLeft - 25;
+            const maxX = boardRef.current.offsetLeft + boardRef.current.clientWidth - 75;
+            const minY = boardRef.current.offsetTop ;
+            const maxY = boardRef.current.offsetTop + boardRef.current.clientHeight - 75;
+            const x = e.clientX - 0.5*(element.clientHeight);
+            const y = e.clientY - 0.5*(element.clientHeight);
+            element.style.position='absolute';
+
+            //restricting pieces within the board horizontally
+            if(x<minX){
+                element.style.left = `${minX}px`;
+            } else if (x>maxX){
+                element.style.left = `${maxX}px`
+            } else {
+                element.style.left = `${x}px`
+            }
+
+            //restricting pieces within the board vertically
+            if(y<minY){
+                element.style.top = `${minY}px`;
+            } else if (y>maxY){
+                element.style.top = `${maxY}px`
+            } else {
+                element.style.top = `${y}px`
+            }
+        }
+    }
+
+    const dropPiece = (e) => {
+        if(active){
+            const x = Math.floor((e.clientX - boardRef.current.offsetLeft) / 100);
+            const y = Math.abs(Math.ceil((e.clientY - boardRef.current.offsetTop - 800) / 100));
+
+            setPieces((value)=> {
+                const new_pieces = value.map((piece)=> { 
+                    if(piece.x === gridX && piece.y === gridY){
+                        piece.x = x;
+                        piece.y = y;
+                    }
+                    return piece;
+                });
+                return new_pieces;
+            });
+            setActive(false);
+        }
+    }
+    //look into why i need returns here!!!!!!!!
+
   return (
     <div className = {styles.fullboard}>
-        <div className={styles.numbers}>
+        <button onClick = {(e) => {console.log(boardRef.current)}}>click</button>
+        {/* <div className={styles.numbers}>
             {numbers}
-        </div>
+        </div> */}
 
-        <div className={styles.chessboard}>
+        <div 
+        onMouseMove={e=> movePiece(e)} 
+        onMouseDown={e => grabPiece(e)} 
+        onMouseUp={e => dropPiece(e)}
+        className={styles.chessboard}
+        ref={boardRef}
+        >
             {Board}
         </div>
 
--- src/Components/Tile.jsx
+++ src/Components/Tile.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Styles/Tile.module.scss';
-
 const Tile = (props) => {
     
     if(props.number % 2 === 0){
-        return <div className={styles.blackTile}><img src={props.image}/></div>;
+        return (
+            <div className={styles.blackTile}>
+                {props.image && <div style={{backgroundImage: `url(${props.image})`}} className={styles.piece}></div>}
+            </div>);
     } else {
-        return <div className={styles.whiteTile}><img src={props.image}/></div>;
+        return (
+            <div className={styles.whiteTile}>
+                {props.image && <div style={{backgroundImage: `url(${props.image})`}} className={styles.piece}></div>}
+            </div>);
     }
 }
 
