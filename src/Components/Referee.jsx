7ab71fbd8e19c7ba096a85d205b4477152df2401
Sajed Azizi <sajedazizi@Sajeds-MacBook-Air.local>
2022-07-27 22:58:58 +0100

    refactored pawn movement code to be clearer

--- src/Components/Referee.jsx
+++ src/Components/Referee.jsx
@@ -3,17 +3,12 @@
 export default class Referee {
 
    tileIsOccupied = (x, y, boardState) => {
-    console.log("checking if tile is occupied...");
-    console.log(boardState.find((p)=> p.x === x && p.y === y));
     const piece = boardState.find((p)=> p.x === x && p.y === y);
-
     if(piece){
       return true;
     } else {
       return false;
     };
-
-    
    }
 
    isValidMove = (px, py, x, y, type, team, boardState) => {
@@ -23,51 +18,20 @@ export default class Referee {
     console.log(`type: ${type}`);
     console.log(`team: ${team}`);
 
-    if(type === "PAWN") {
-      if(team === "WHITE") {
-        if(py === 1) {
-          if(px === x && y - py ===1){
-            if(!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-             }
-            } else if (px === x && y - py === 2) {
-              if(!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y-1, boardState)) {
-                return true;
-              }
-            }
-        } else {
-          if(px === x && (y - py === 1)) {
-            if(!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-            } else{
-              return false;
-            }
-            
-          }
-        }
-      } else {
-        if(py === 6) {
-          if(px === x && py - y === 1 ) {
-            if(!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-            }
-          } else if (px === x && py - y === 2){
-            if(!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y+1, boardState)) {
-              return true;
-            }
-          }
-        } else {
-          if(px === x && (py - y === 1)) {
-            if(!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-            }
-          }
-        }
+    if(type === "PAWN"){
+     const pawnRow = (team === "WHITE") ? 1: 6;
+     const pawnDirection = (team === "WHITE") ? 1: -1;
+
+     if(py === pawnRow && y - py === 2*pawnDirection){
+      if(!this.tileIsOccupied(x, y, boardState) && !this.tileIsOccupied(x, y - pawnDirection, boardState)) {
+        return true;
       }
-
+     } else if(px === x && y - py === pawnDirection){
+      if(!this.tileIsOccupied(x, y, boardState)){
+        return true;
+      }
+     }
     }
-
     return false;
-
   }
 }
